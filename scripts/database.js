d48b794a4b87d02fe2e78b51613e011632940ed4
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-20 15:16:20 -0600

    working, minus css edits

--- scripts/database.js
+++ scripts/database.js
@@ -1,15 +1,61 @@
 const database = {
     fish: [
         {
-            housename: ["Bart", "Angie", "Reggie"],
-            species: ["Angelfish", "Discus fish", "Butterflyfish"],
-            food: ["crustaceans", "algae", "plankton"],
-            size: [3, 2, 1],
-            harvested: ["South America", "South America", "Indian Ocean"],
-            freshwater: [true, true, false]
+            image: "https://i.pinimg.com/originals/03/43/54/034354547e15a230812ce191f62b7311.jpg",
+            name: "Bart",
+            species: "Angelfish",
+            length: 3,
+            location: "South America",
+            food: "crustaceans"
+        },
+        {
+            image: "https://img.thepets.net/wp-content/uploads/discus-fish-large.jpg",
+            name: "Angie",
+            species: "Discus fish",
+            length: 2,
+            location: "South America",
+            food: "algae"
+        },
+        {
+            image: "https://img.thepets.net/wp-content/uploads/discus-fish-large.jpg",
+            name: "Reggie",
+            species: "Butterfly fish",
+            length: 1,
+            location: "Indian Ocean",
+            food: "plankton"
         }
     ]
 }
 export const getFish = () => {
     return database.fish.map(fish => ({...fish}))
+}
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+    for (const fish of database.fish) {
+        if (fish.length % 3 === 0)
+        holyFish.push(fish.name)
+    }
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = []
+    for (const fish of database.fish) {
+        if (fish.length % 5 === 0)
+        soldiers.push(fish.name)
+    }
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish = []
+    for (const fish of database.fish) {
+        if (fish.length % 5 != 0 && fish.length % 3 != 0)
+        regularFish.push(fish.name)
+    }
+    return regularFish
 }
